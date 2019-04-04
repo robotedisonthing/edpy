dd3c77fce8031203ce3b9797018591defc905496
robotedisonthing <49283707+robotedisonthing@users.noreply.github.com>
2019-04-04 10:17:45 -0700

    Update robotedpy.py

--- robotedpy.py
+++ robotedpy.py
@@ -11,26 +11,22 @@ Ed.Tempo = Ed.TEMPO_MEDIUM
 #--------Your code below-----------
 
 Ed.LineTrackerLed(Ed.ON)
-clap = 0
+Ed.ObstacleDetectionBeam(Ed.ON)
 obstacle = 0
 
 while True:
-    if clap == 0:
-        waitClap()
-        clap = 1
-        if Ed.ReadLineState()==Ed.LINE_ON_WHITE:
-            Ed.Drive(Ed.FORWARD_RIGHT, Ed.SPEED_1, Ed.DISTANCE_UNLIMITED)
-        else:
-            Ed.Drive(Ed.FORWARD_LEFT, Ed.SPEED_1, Ed.DISTANCE_UNLIMITED)
-    obstacle = Ed.ReadObstacleDetection()
-    if obstacle>Ed.OBSTACLE_NONE:
+	if Ed.ReadLineState()==Ed.LINE_ON_WHITE:
+		Ed.Drive(Ed.FORWARD_RIGHT, Ed.SPEED_5, Ed.DISTANCE_UNLIMITED)
+	else:
+		Ed.Drive(Ed.FORWARD_LEFT, Ed.SPEED_5, Ed.DISTANCE_UNLIMITED)
+	obstacle = Ed.ReadObstacleDetection()
+	if obstacle>Ed.OBSTACLE_NONE:
 		Ed.LeftLed(Ed.ON)
 		Ed.RightLed(Ed.ON)
 		Ed.PlayBeep()
+		Ed.Drive(Ed.STOP, Ed.SPEED_1, 0)
 		if obstacle==Ed.OBSTACLE_AHEAD:
-		    Ed.Drive(Ed.STOP, Ed.SPEED_1, 0)
-		    Ed.TimeWait(3, Ed.TIME_SECONDS)
-
-def waitClap():
-	while Ed.ReadClapSensor() != Ed.CLAP_DETECTED:
-		pass
+			Ed.Drive(Ed.STOP, Ed.SPEED_1, 0)
+			Ed.TimeWait(3, Ed.Tb955IME_SECONDS)
+	Ed.LeftLed(Ed.OFF)
+	Ed.RightLed(Ed.OFF)
